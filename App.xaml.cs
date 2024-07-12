1d2d70cf52e46d07093edba8f5f2f7c09ef4a1bb
Long Doãn Văn <doanlong266@gmail.com>
2024-07-12 17:57:50 +0700

    add new project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieNest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieNest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
