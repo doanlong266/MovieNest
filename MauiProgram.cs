3ee9a6378ab9378dbc7191a0542572a519554a40
Long Doãn Văn <doanlong266@gmail.com>
2024-07-12 18:48:38 +0700

    add font

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,12 +11,26 @@ namespace MovieNest
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+                    fonts.AddFont("fa-duotone-900.ttf", "FAD");
+                    fonts.AddFont("fa-light-300.ttf", "FAL");
+                    fonts.AddFont("fa-thin-100.ttf", "FAT");
+                    fonts.AddFont("fa-brands.ttf", "FAB");
+                    fonts.AddFont("fa-regular.ttf", "FAR");
+                    fonts.AddFont("fa-solid.ttf", "FAS");
+                    fonts.AddFont("Lato-Black.ttf", "LB");
+                    fonts.AddFont("Lato-BlackItalic.ttf", "LBI");
+                    fonts.AddFont("Lato-Bold.ttf", "LBO");
+                    fonts.AddFont("Lato-BoldItalic.ttf", "LBOI");
+                    fonts.AddFont("Lato-Italic.ttf", "LI");
+                    fonts.AddFont("Lato-Light.ttf", "LL");
+                    fonts.AddFont("Lato-LightItalic.ttf", "LLI");
+                    fonts.AddFont("Lato-Regular.ttf", "LR");
+                    fonts.AddFont("Lato-Thin.ttf", "LT");
+                    fonts.AddFont("Lato-ThinItalic.ttf","LTI");
+                }); 
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
